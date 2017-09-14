11446e1a67ee469239113ea3fa94caee05c70101
Ambas Chobsanti <ambas.chobsanti@lazada.com>
2017-09-14 12:12:15 +0700

    Firt commit

---
+++ RichNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RichNotification
+//
+//  Created by Ambas Chobsanti (Lazada Group) on 9/14/17.
+//  Copyright © 2017 Ambas Chobsanti (Lazada Group). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
